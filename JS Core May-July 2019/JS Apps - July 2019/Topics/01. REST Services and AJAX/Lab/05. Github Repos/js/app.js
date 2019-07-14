a1a7bf0ae3a396dcb8bf5283f1947227545839c5
kaneva91 <kaneva91@gmail.com>
2019-07-14 18:13:28 +0300

    Modified SoftUniStudentJS\JS Core May-July 2019\JS Apps - July 2019\Topics\01. REST Services and AJAX\Lab\05. Github Repos

--- JS Core May-July 2019/JS Apps - July 2019/Topics/01. REST Services and AJAX/Lab/05. Github Repos/js/app.js
+++ JS Core May-July 2019/JS Apps - July 2019/Topics/01. REST Services and AJAX/Lab/05. Github Repos/js/app.js
@@ -1,42 +1,43 @@
-window.onload = (function(){
+window.onload = (function () {
     let input = document.querySelector('#input-text'),
-    ul = document.querySelector('ul'),
-    btn = document.querySelector('button');
-   
+        ul = document.querySelector('ul'),
+        btn = document.querySelector('button');
+
     btn.addEventListener('click', eventHandler);
 
 
-    function eventHandler(){
-       if(input.value !== ''){
+    function eventHandler() {
+        if (input.value !== '') {
+            ul.innerHTML = '';
+
             const username = input.value;
             input.value = '';
 
             fetch(`https://api.github.com/users/${username}/repos`)
-            .then(response => isSuccessfullToBeParsed(response))
-            .then(repos => printRepos(repos)); 
-       }
+                .then(response => isSuccessfullToBeParsed(response))
+                .then(repos => printRepos(repos));
+        }
     }
 
 
-
-    function isSuccessfullToBeParsed(response){
-        if(!response.status === 200){
+    function isSuccessfullToBeParsed(response) {
+        if (!response.status === 200) {
             return;
         }
         return response.json();
     }
-    
-    function printRepos(repos){
-      
-       repos.forEach(repo => {
-           appendToList(repo.owner.login, repo.html_url, repo.name);
-           
-       });
+
+    function printRepos(repos) {
+
+        repos.forEach(repo => {
+            appendToList(repo.owner.login, repo.html_url, repo.name);
+
+        });
     }
 
-    function appendToList(username, url, repoName){
+    function appendToList(username, url, repoName) {
         let li = document.createElement('li'),
-        a = document.createElement('a');
+            a = document.createElement('a');
 
         a.setAttribute('href', url);
         a.setAttribute('target', '_blank');
