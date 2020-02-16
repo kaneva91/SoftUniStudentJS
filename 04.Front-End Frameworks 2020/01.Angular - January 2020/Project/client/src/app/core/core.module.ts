4b2df674e26e0dd43880c466ba9d96b461c8dee1
Kalina Kaneva <kaneva91@gmail.com>
2020-02-16 15:50:50 +0200

    Auth guard added

--- 04.Front-End Frameworks 2020/01.Angular - January 2020/Project/client/src/app/core/core.module.ts
+++ 04.Front-End Frameworks 2020/01.Angular - January 2020/Project/client/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppInterceptor } from '../app-interceptor';
+import { AuthGuard } from './guards/auth.guard';
 
 
 
 @NgModule({
   declarations: [],
+  providers :[AuthGuard],
   imports: [
     CommonModule
   ],
--- 04.Front-End Frameworks 2020/01.Angular - January 2020/Project/client/src/app/user/user-routing.module.ts
+++ 04.Front-End Frameworks 2020/01.Angular - January 2020/Project/client/src/app/user/user-routing.module.ts
@@ -3,29 +3,50 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 
 const routes: Routes = [
   {
     path: 'user',
-    children : [
-        {
-            path: '',
-            pathMatch : 'full',
-            redirectTo : '/'
-        },
-        {
-            path : 'login',
-            component : LoginComponent
-        },
-        {
-            path: 'register',
-            component : RegisterComponent
-        },
-        {
-          path: 'profile',
-          component : ProfileComponent
-        },
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/'
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        canActivate: [AuthGuard],
+        data: {
+          isLogged: false
+        }
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        canActivate: [AuthGuard],
+        data: {
+          isLogged: false
+        }
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [AuthGuard],
+        data: {
+          isLogged: true
+        }
+      },
+      {
+        path: 'cart',
+        component: ProfileComponent,
+        canActivate: [AuthGuard],
+        data: {
+          isLogged: true
+        }
+      },
     ]
   }
 ];
