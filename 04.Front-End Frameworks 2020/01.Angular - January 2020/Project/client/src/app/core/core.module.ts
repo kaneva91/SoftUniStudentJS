7547a528a363ed728562d1b1923d33dbee64c732
Kalina Kaneva <kaneva91@gmail.com>
2020-02-20 23:15:22 +0200

    category-list heading fixes

--- 04.Front-End Frameworks 2020/01.Angular - January 2020/Project/client/src/app/core/core.module.ts
+++ 04.Front-End Frameworks 2020/01.Angular - January 2020/Project/client/src/app/core/core.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthGuard } from './guards/auth.guard';
 
-
-
-
 @NgModule({
   declarations: [],
   providers :[AuthGuard],
