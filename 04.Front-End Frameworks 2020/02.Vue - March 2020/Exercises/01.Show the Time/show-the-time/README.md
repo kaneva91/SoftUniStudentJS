87d8014b2187ef0968d25d8bad38509493307720
Kalina Kaneva <kaneva91@gmail.com>
2020-03-17 19:06:49 +0200

    Show the Time exercise added

---
+++ 04.Front-End Frameworks 2020/02.Vue - March 2020/Exercises/01.Show the Time/show-the-time/README.md
@@ -0,0 +1,26 @@
+# show-the-time
+
+A simple application whichs shows the local time when the heading is hovered.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ 04.Front-End Frameworks 2020/02.Vue - March 2020/Exercises/01.Show the Time/show-the-time/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 @mouseover="showTheTime" @mouseleave="hideTheTime">
+      The time is... ?
+      <span>(hover)</span>
+    </h1>
+    <p v-show="isDateHidden">{{date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      isDateHidden: false,
+      date: ''
+    };
+  },
+
+  methods: {
+    showTheTime() {
+      this.isDateHidden = true;
+      setInterval(() => {
+        this.date = new Date();
+      }, 1000);
+    },
+    hideTheTime() {
+      this.isDateHidden = false;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+h1 {
+  margin-top: 200px;
+}
+span {
+  font-style: italic;
+  font-size: 20px;
+}
+</style>
