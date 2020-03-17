125bbb3601c11d9e38d0b6eccef76046d4025ecd
Kalina Kaneva <kaneva91@gmail.com>
2020-03-17 19:53:13 +0200

    Product List exercise added

---
+++ 04.Front-End Frameworks 2020/02.Vue - March 2020/Exercises/02.Product List/product-list/README.md
@@ -0,0 +1,25 @@
+# product-list
+The application represents conditional rendering exercise of list of products. Depending on the inedex of the products if it is odd or even different CSS styles are being applied to.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ 04.Front-End Frameworks 2020/02.Vue - March 2020/Exercises/02.Product List/product-list/src/components/ProductList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <ul>
+      <li v-for="(product, index) of products" v-bind:key="index" :class="oddOrEven(index)">
+        {{product}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  data: function() {
+    return {
+      products: ["Cucumber", "Grape", "Carrot", "Banana", "Patato", "Apple"]
+    };
+  },
+  methods : {
+    oddOrEven : function(index){
+      return index % 2 === 0 ? 'odd' : 'even';
+    }
+  }
+};
+</script>
+ 
+
+<style scoped>
+ul {
+  list-style-type: none;
+  font-size: 18px;
+}
+li {
+  border: 2px solid black;
+  padding: 5px;
+  width: 20%;
+  margin: 6px auto;
+}
+.odd{
+  background: yellow;
+  font-style: italic;
+}
+.even{
+  background: cyan;
+  border: 2px solid red;
+}
+</style>
