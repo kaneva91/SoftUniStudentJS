981ffa26ae83f3e069ec1643f02672fa6ddad3d0
Kalina Kaneva <kaneva91@gmail.com>
2020-04-06 14:31:14 +0300

    Foms Workshop Apped

---
+++ 04.Front-End Frameworks 2020/02.Vue - March 2020/Workshops/Part 2/forms/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+   <input type="text" v-model="input"/>
+   {{input | reverse}}
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function(){
+    return {
+      input : ''
+    }
+  },
+  filters : {
+    reverse(value){
+      return value.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
